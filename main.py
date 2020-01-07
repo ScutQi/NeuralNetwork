34dfc295d65b517708afa5055da05a4ed394aabb
ScutQi <scutqi@gmail.com>
2020-01-07 13:33:25 +0800

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class NetWork:
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learn_rate):
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+        self.learn_rate = learn_rate
+
+    def train(self):
+        pass
+
+    def query(self):
+        pass
+
+
+if __name__ == 'main':
+    pass
